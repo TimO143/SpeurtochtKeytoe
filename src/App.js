b079039a3b1b61a1bac9dad496cf0994ebfd858f
Branko <0928268@hr.nl>
2019-09-06 14:27:16 +0200

    CSS + All Branko files
    Pull whats necessary

--- src/App.js
+++ src/App.js
@@ -102,7 +102,6 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Quiz</h2>
         </div>
         {this.state.result ? this.renderResult() : this.renderQuiz()}
       </div>
--- src/components/AnswerOption.js
+++ src/components/AnswerOption.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const errorStyle = { color: 'red' }
-const Input = ({ label, error, onChange, name, value }) => (
+const input = ({ label, error, onChange, name, value }) => (
     <label>
-        {label}:
+        {label}
     <input name={name} type='text' value={value} onChange={onChange} required />
         {error && <span style={errorStyle}>{error}</span>}
     </label>
@@ -22,25 +22,25 @@ class AnswerOption extends React.Component {
     }
     onChange = (e) => {
         this.setState({
-            value: e.target.value
+            value: e.target.value.toLowerCase()
         })
     }
 
     render() {
         return (
-            <li className="answerOption">
+            <div className="answerOption">
                 <form onSubmit={e => { e.preventDefault(); this.props.onAnswerSelected(this.state.value)}}>
-                    <Input
+                    <input
                         type='text'
+                        placeholder="Je antwoord"
                         //value={form.userName}
                         onChange={this.onChange}
                         //error={asyncErrors.userName}
                     />
-                    <input type="submit" value="Submit"
-                    />
+                    
                 </form>
 
-            </li>
+            </div>
         );
     }
 }
@@ -53,57 +53,5 @@ AnswerOption.propTypes = {
   
 };
 
-
-        };
-    }
-
-    handleAnswerChange = evt => {
-        this.setState({ answer: evt.target.value });
-    };
-    handleSubmit = evt => {
-        if (!this.canBeSubmitted()) {
-            evt.preventDefault();
-            return;
-        }
-        const { answer } = this.state;
-        alert('success');
-    };
-
-    canBeSubmitted() {
-        const error = validate(this.state.answer);
-        const isDisabled = Object.keys(error).some(x => error[x]);
-    }
-
-    render() {
-        const error = validate(this.state.answer);
-        const isDisabled = Object.keys(error).some(x => error[x]);
-        return(
-        <form onSubmit={this.handleSubmit}>
-            <input
-                className={error.answer ? "error" : ""}
-                type="text"
-                placeholder="ANSWER HERE"
-                value={this.state.answer}
-                onChange={this.handleAnswerChange}
-            />
-            <button disabled={isDisabled}>SUBMIT</button>
-        </form>
-            );
-        }
-
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<AnswerSubmit />, rootElement);
-
-//AnswerOption.propTypes = {
-//  //answerType: PropTypes.string.isRequired,
-//  //answerContent: PropTypes.string.isRequired,
-//  answer: PropTypes.string.isRequired,
-//  onAnswerSelected: PropTypes.func.isRequired
-//};
-
-export default AnswerSubmit;
-
-import PropTypes from 'prop-types';
-import quizQuestions from '../api/quizQuestions';
+export default AnswerOption;
+     
