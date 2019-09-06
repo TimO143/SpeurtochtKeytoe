27226c444cdfd5534c70bd4b986af3c0f056d452
TimOosterlee <0929381@hr.nl>
2019-09-06 11:58:06 +0200

    submit werkt
    nog geen opmaak bij error message

--- src/components/AnswerOption.js
+++ src/components/AnswerOption.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import quizQuestions from '../api/quizQuestions';
+
+const errorStyle = { color: 'red' }
+const Input = ({ label, error, onChange, name, value }) => (
+    <label>
+        {label}:
+    <input name={name} type='text' value={value} onChange={onChange} required />
+        {error && <span style={errorStyle}>{error}</span>}
+    </label>
+    )
+
+
 
 class AnswerOption extends React.Component {
 
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+           value: null
+        }
     }
+    onChange = (e) => {
+        this.setState({
+            value: e.target.value
+        })
+    }
+
     render() {
         return (
             <li className="answerOption">
-                <form onSubmit={this.props.onAnswerSelected}>
-                    <input
-                        type="input"
-                    //value={this.props.givenAnswer}
+                <form onSubmit={e => { e.preventDefault(); this.props.onAnswerSelected(this.state.value)}}>
+                    <Input
+                        type='text'
+                        //value={form.userName}
+                        onChange={this.onChange}
+                        //error={asyncErrors.userName}
+                    />
+                    <input type="submit" value="Submit"
                     />
                 </form>
 
